a89b1f0f1a17765fcf957cec5d1af08c7e53c57f
Gui J Vale <gui.vale18@hotmail.com>
2022-03-11 18:06:34 -0800

    first commit

---
+++ docusaurus.config.js
@@ -0,0 +1,57 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "SeedCMS",
+  tagline: "",
+  url: "https://seedcms.github.io/",
+  baseUrl: "/docs/",
+  onBrokenLinks: "throw",
+  trailingSlash: false,
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "seedcms", // Usually your GitHub org/user name.
+  projectName: "docs", // Usually your repo name.
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          routeBasePath: "/",
+          // Please change this to your repo.
+          editUrl: "https://github.com/seedcms/docs",
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "CS Core",
+        items: [],
+      },
+      footer: {
+        style: "dark",
+        copyright: `Copyright © ${new Date().getFullYear()} documentation SeedCMS.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
