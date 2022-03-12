0eac3d10b577325667d07b4145f4f12d4a9e89d7
Gui J Vale <gui.vale18@hotmail.com>
2022-03-12 11:35:38 -0800

    updated config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "SeedCMS",
+  title: "CS CORE",
   tagline: "",
   url: "https://seedcms.github.io/",
   baseUrl: "/cs-core/",
